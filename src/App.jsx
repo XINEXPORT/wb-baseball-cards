d72a91dd478fee6d423d1f4b1fd929e21139e5f1
Christine Hoang <christinehoangt@gmail.com>
2023-11-27 14:36:20 -0600

    front-complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import playerData from './playerData.js'
+
+
+
 function BaseballCard(props) {
+  let {name, team, position, stats, imgUrl, cardId} = props
+  console.log(props);
   return (
     <div className="card">
-      <h2>Player Name goes here</h2>
+      <h2>{props.name}</h2>
+      <img src = {props.imgUrl}></img>
     </div>
   );
 }
 
 function App() {
-  return <BaseballCard />;
+  const cards = playerData.map(({name, team, position, stats, imgUrl, cardId})=>
+  < BaseballCard
+  name = {name}
+  team = {team}
+  position = {position}
+  stats = {stats}
+  imgUrl = {imgUrl}
+  cardId = {cardId}
+  banana="hi"/>);
+  return <>{cards}</>;
 }
 
 export default App;
