b4b8018d6f7b502e90424c16c8deced7c3fdd97d
Christine Hoang <christinehoangt@gmail.com>
2023-11-27 15:30:43 -0600

    project complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
 import playerData from './playerData.js'
+import { useState } from 'react';
 
 
 
 function BaseballCard(props) {
+  
+  const [showPicture, setShowPicture] = useState(true);
   let {name, team, position, stats, imgUrl, cardId} = props
-  console.log(props);
+
+  function toggleCard(){
+    setShowPicture(!showPicture);
+  }
+
+if (showPicture) {
   return (
-    <div className="card">
+    <div className="card" onClick={toggleCard}>
       <h2>{props.name}</h2>
       <img src = {props.imgUrl}></img>
     </div>
-  );
-}
+  );} else{
+
+  const statsDisplay = [];
+  for (let stat in stats){
+    statsDisplay.push(<p>{stat}: {stats[stat]}</p>)
+  }
+  return(
+    <div className="card" onClick={toggleCard}>
+      <h2>{props.name}</h2>
+      <p>{props.team}</p>
+      <p>{props.position}</p>
+      {statsDisplay}
+    </div>
+  )
+}}
 
 function App() {
   const cards = playerData.map(({name, team, position, stats, imgUrl, cardId})=>
@@ -22,7 +43,8 @@ function App() {
   stats = {stats}
   imgUrl = {imgUrl}
   cardId = {cardId}
-  banana="hi"/>);
+  key={cardId}
+  />);
   return <>{cards}</>;
 }
 
